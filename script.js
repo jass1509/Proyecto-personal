91191a392d455cd373e793cc0f8cc2a2852b3743
jass1509 <https://www.google.com/support/accounts/bin/answer.py?answer=181692>
2025-12-21 12:44:54 -0500

    mejorando menus aladiendo contador y cambiar el tema

--- script.js
+++ script.js
@@ -2,4 +2,28 @@
   function toggleMenu(){
     document.getElementById('sideMenu').classList.toggle('open');
   }
+  const links = document.querySelectorAll("nav a");
+  const actual = location.pathname.split("/").pop();
 
+  links.forEach(link => {
+    if (link.getAttribute("href") === actual) {
+      link.classList.add("active");
+    }
+  });
+
+  // Contador de visitas
+  let visitas = localStorage.getItem("visitas") || 0;
+visitas++;
+localStorage.setItem("visitas", visitas);
+document.getElementById("contador").textContent = visitas;
+
+// Cambiar tema
+function cambiarTema(){
+  document.body.classList.toggle("dark");
+  localStorage.setItem("tema",
+    document.body.classList.contains("dark") ? "dark" : "light");
+}
+
+if(localStorage.getItem("tema") === "dark"){
+  document.body.classList.add("dark");
+}
